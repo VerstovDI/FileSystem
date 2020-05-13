e97cbe14747c5c64b76acb8501af511bafab2585
Ovoshi <ston8@mail.ru>
2020-05-13 23:55:46 +0300

    save&load

--- FIleSystem/FileSystemProject/src/main/java/Functions/DownloadFS/DownloadFSCommand.java
+++ FIleSystem/FileSystemProject/src/main/java/Functions/DownloadFS/DownloadFSCommand.java
@@ -1,23 +1,58 @@
 package Functions.DownloadFS;
 
+import Functions.ParameterReaders.ReadParameters;
 import Structure.FileSystemStructure.FileSystem;
 import Structure.FileSystemStructure.ICommand;
 import Structure.FileSystemStructure.IMessageWriter;
 import Structure.FileSystemStructure.IParameterReader;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+
 public class DownloadFSCommand implements ICommand {
-    DownloadFSParameters par;
+    String fileName;
     @Override
     public void Execute(FileSystem fs, IParameterReader parameter, IMessageWriter message) {
-
-
+        ReadParameters(parameter);
+        try {
+            FileSystem fsbuf = null;
+            fsbuf=this.Download(fileName);
+            fs.copy(fsbuf);
+        }
+        catch (ClassNotFoundException ex){
+            System.out.println(ex);
+        }
+        catch (IOException exio){
+            System.out.println(exio);
+        }
     }
 
     @Override
     public void ReadParameters(IParameterReader parameterReader) {
-    IParameterReader p =parameterReader.ParameterReader();
-        DownloadFSParameters par;
-        par =(DownloadFSParameters) p;
-        this.par=par;
+        this.fileName=(((ReadParameters) parameterReader).readFileName("Введите имя файла для загрузки информации: "));
+    }
+
+
+
+    public  FileSystem Download( String filename) throws IOException, ClassNotFoundException {
+
+        FileInputStream fi = new FileInputStream(new File(filename+".txt"));
+        ObjectInputStream oi = new ObjectInputStream(fi);
+        FileSystem fs = (FileSystem) oi.readObject();//считали параметры FS
+
+        /*
+        CreateFileCommand createFileCommand=new CreateFileCommand();
+        String line=null;
+        BufferedReader br = new BufferedReader(new FileReader(filename+".txt"));
+        while ((line = br.readLine()) != null) {
+            createFileCommand.createFile(fs,(DataInfo)oi.readObject());
+        }
+        */
+        oi.close();
+        fi.close();
+        return fs;
     }
 }
--- FIleSystem/FileSystemProject/src/main/java/Structure/FileSystemStructure/FileSystem.java
+++ FIleSystem/FileSystemProject/src/main/java/Structure/FileSystemStructure/FileSystem.java
@@ -1,9 +1,10 @@
 package Structure.FileSystemStructure;
 
 import java.io.PrintWriter;
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class FileSystem {
+public class FileSystem implements Serializable {
     public static PrintWriter out;
     public String systemName;           // Имя ФС
     public String ownerName;            // Владелец ФС
@@ -12,7 +13,7 @@ public class FileSystem {
     public ArrayList<Segment> seg;      // Массив сегментов ФС. В системе может быть не более Segment.segmentsLimit (31) сегмента.
     public int cntOfSegments;           // Желаемое число сегментов в каталоге (оно обязано быть <= Segment.segmentsLimit).
 
-   public FileSystem(int size, String systemName, String ownerName, int tomId, int version){
+    public FileSystem(int size, String systemName, String ownerName, int tomId, int version){
         this.systemName = systemName;
         this.ownerName = ownerName;
         this.tomId = tomId;
@@ -21,5 +22,23 @@ public class FileSystem {
         ArrayList<Segment> buf = new ArrayList<Segment>();
         buf.ensureCapacity(size);
         seg = buf;
+    }
+    @Override
+    public String  toString () {
+        return "systemName:"+ systemName+
+                "\nownerName:"+ownerName+
+                "\ntomId:"+tomId+
+                "\nversion:"+version+
+                // "\nseg:"+seg.toString()+
+                "\ncntOfSegments:"+cntOfSegments;
+    }
+
+    public void copy(FileSystem fsbuf){
+        this.systemName = fsbuf.systemName;
+        this.ownerName =fsbuf. ownerName;
+        this.tomId = fsbuf.tomId;
+        this.version = fsbuf.version;
+        this.cntOfSegments = fsbuf.cntOfSegments;
+        this.seg = fsbuf.seg;
     }
 }
